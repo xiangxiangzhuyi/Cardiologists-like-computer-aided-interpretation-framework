cb0c3f8c78b5e96c47a30f1f4ea08631ee1d17dd
xiangxiangzhuyi <hlttbnn@foxmail.com>
2023-05-06 15:37:37 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import run
 import argparse
+
 # pass parameters to the script
 pa = argparse.ArgumentParser(description='manual to this script')
 pa.add_argument('--ty_n', type=int, default = None)
